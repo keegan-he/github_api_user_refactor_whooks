f059a94ee95cea2c2a14667347baac678737de6c
Keegan H <keeganslane@gmail.com>
2019-09-22 19:40:49 -0700

    cleaning

--- src/App.js
+++ src/App.js
@@ -20,12 +20,10 @@ const App = () => {
   //Search Github users:
   const searchUsers = async text => {
     setLoading(true);
-
     const res = await axios.get(
       `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
     {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
-
     setUsers(res.data.items);
     setLoading(false);
   };
@@ -33,12 +31,10 @@ const App = () => {
   //Get Single Github user:
   const getUser = async username => {
     setLoading(true);
-
     const res = await axios.get(
       `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
   {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
-
     setUser(res.data);
     setLoading(false);
   };
@@ -46,12 +42,10 @@ const App = () => {
   // Get User's Repos:
   const getUserRepos = async username => {
     setLoading(true);
-
     const res = await axios.get(
       `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
   {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
-
     setRepos(res.data);
     setLoading(false);
   };
