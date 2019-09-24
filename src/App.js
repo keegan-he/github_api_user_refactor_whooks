7f997d589a9d740805497f98c12073d3c0e207ed
Keegan H <keeganslane@gmail.com>
2019-09-24 14:55:19 -0700

    not found route added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import User from './components/users/User';
@@ -7,6 +7,7 @@ import About from './components/pages/About';
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
 import Home from './components/pages/Home';
+import NotFound from './components/pages/NotFound';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import './App.css';
 
@@ -23,6 +24,7 @@ const App = () => {
                 <Route exact path="/" component={Home} />
                 <Route exact path="/about" component={About} />
                 <Route exact path="/user/:login" component={User} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
