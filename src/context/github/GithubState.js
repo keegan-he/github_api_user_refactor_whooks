30b1a34907d73322ad1d18e4350de9f8294775a8
Keegan H <keeganslane@gmail.com>
2019-09-24 13:50:31 -0700

    User.js update

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -38,6 +38,19 @@ const GithubState = props => {
 
   // GET USER
 
+  const getUser = async username => {
+    setLoading();
+    const res = await axios.get(
+      `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
+  {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    dispatch({
+      type: GET_USER,
+      payload: res.data
+    });
+  };
+
   // GET REPOS
 
   // CLEAR USERS
@@ -55,7 +68,8 @@ const GithubState = props => {
         repos: state.repos,
         loading: state.loading,
         searchUsers,
-        clearUsers
+        clearUsers,
+        getUser
       }}
     >
       {props.children}
