d48be46bef4f5067cf1270278b095452bd2cf2cd
Keegan H <keeganslane@gmail.com>
2019-09-24 14:50:33 -0700

    tidying up

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import './App.css';
 import Navbar from './components/layout/Navbar';
 import User from './components/users/User';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
+import Home from './components/pages/Home';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import './App.css';
 
 const App = () => {
   return (
@@ -19,16 +20,7 @@ const App = () => {
             <div className="container">
               <Alert />
               <Switch>
-                <Route
-                  exact
-                  path="/"
-                  render={props => (
-                    <Fragment>
-                      <Search />
-                      <Users />
-                    </Fragment>
-                  )}
-                />
+                <Route exact path="/" component={Home} />
                 <Route exact path="/about" component={About} />
                 <Route exact path="/user/:login" component={User} />
               </Switch>
